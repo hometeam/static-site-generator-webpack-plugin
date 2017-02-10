f99c243b604141b6ca1686a896812c06cf7ac644
Adrian <adrian@hometeamcare.com>
2017-02-09 19:32:29 -0500

    v1

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var RawSource = require('webpack-sources/lib/RawSource');
 var evaluate = require('eval');
 var path = require('path');
 var Promise = require('bluebird');
+var fs = require('fs');
 
 function StaticSiteGeneratorWebpackPlugin(renderSrc, outputPaths, locals, scope, options) {
   this.renderSrc = renderSrc;
@@ -11,12 +12,41 @@ function StaticSiteGeneratorWebpackPlugin(renderSrc, outputPaths, locals, scope,
   this.scope = scope;
 }
 
+//TODO Make this prettier.
+function addAssets(file, options, locals) {
+
+  const scripts = [
+    '<script src="' + locals.staticFolder + '/main.bundle.js" ></script>',
+  ];
+
+  const styles = [
+    '<link rel="stylesheet" href="' + locals.staticFolder + '/main.less-bundle.css" />'
+  ];
+
+  var fileWithAssets = file.replace('<!-- scripts -->', scripts.join(' '));
+  fileWithAssets = fileWithAssets.replace('<!-- styles -->', styles.join(' '));
+
+  return fileWithAssets;
+}
+
 
 StaticSiteGeneratorWebpackPlugin.prototype.apply = function(compiler) {
   var self = this;
 
   compiler.plugin('this-compilation', function(compilation) {
+
+    console.log('this-COMPILATION', compilation, compilation.compiler, compilation.compiler.outputPath);
+
+    try {
+      var file = fs.readFileSync('index.html.tpl', "utf8");
+      console.log(file);
+    } catch (err) {
+      console.log('failed');
+    }
+
     compilation.plugin('optimize-assets', function(_, done) {
+
+
       var renderPromises;
 
       var webpackStats = compilation.getStats();
@@ -31,6 +61,8 @@ StaticSiteGeneratorWebpackPlugin.prototype.apply = function(compiler) {
 
         var assets = getAssetsFromCompilation(compilation, webpackStatsJson);
 
+        file = addAssets(file, self.options, self.locals);
+
         var source = asset.source();
         var render = evaluate(source, /* filename: */ self.renderSrc, /* scope: */ self.scope, /* includeGlobals: */ true);
 
@@ -44,6 +76,8 @@ StaticSiteGeneratorWebpackPlugin.prototype.apply = function(compiler) {
 
         renderPromises = self.outputPaths.map(function(outputPath) {
           var outputFileName = outputPath.replace(/^(\/|\\)/, ''); // Remove leading slashes for webpack-dev-server
+
+          console.log(outputFileName);
 
           if (!/\.(html?)$/i.test(outputFileName)) {
             outputFileName = path.join(self.options.outputPathPrefix || '', self.options.outputRewriteFunction(outputFileName) || outputFileName);
@@ -67,7 +101,8 @@ StaticSiteGeneratorWebpackPlugin.prototype.apply = function(compiler) {
 
           return renderPromise
             .then(function(output) {
-              compilation.assets[outputFileName] = new RawSource(output);
+              var newOutput = file.replace('<!-- content -->', output);
+              compilation.assets[outputFileName] = new RawSource(newOutput);
             })
             .catch(function(err) {
               compilation.errors.push(err.stack);
